fd0883035052441e12ef55ed3b196fffe0938b3c
Kacper Kaskow <kacper.kaskow@allegro.pl>
2019-12-01 20:40:45 +0100

    Solve task 1 of day 1

---
+++ day_1/task_1/solution.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+	numbs := readInput()
+	var result int64 = 0
+	for _, v := range numbs {
+		result += int64(calcFuel(v))
+	}
+	fmt.Println(result)
+}
+
+func calcFuel(mass int) int {
+	return (mass / 3) - 2
+}
+
+func readInput() []int {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var result []int
+	for scanner.Scan() {
+		asInt, _ := strconv.Atoi(scanner.Text())
+		result = append(result, asInt)
+	}
+	return result
+}
